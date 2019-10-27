45bb8bbeb328dd6b6751d9ac15edfd31a7473ad6
Gervasio Marchand <github@gervas.io>
2019-10-27 00:24:18 -0300

    Use cloud-init

--- agent.tf
+++ agent.tf
@@ -6,9 +6,10 @@ resource "digitalocean_droplet" "agent" {
     ssh_keys = [
       "${var.ssh_fingerprint}"
     ]
+    user_data = "${file("user_data.yml")}"
 
   connection {
-      user = "root"
+      user = "agent"
       type = "ssh"
       private_key = "${var.ssh_pvt_key}"
       timeout = "2m"
@@ -17,35 +18,8 @@ resource "digitalocean_droplet" "agent" {
 
   provisioner "remote-exec" {
     inline = [
-      # create the agent user
-      "adduser --disabled-password --gecos \"\" agent",
-      "usermod -aG sudo agent",
-      "echo 'agent ALL=(ALL) NOPASSWD:ALL' | EDITOR='tee -a' visudo",
-
-      # run all of what's next as the 'agent' user
-      "sudo -H -i -u agent bash << EOF",
-      "cd ~",
-
-      # install powershell
-      "wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb",
-      "sudo dpkg -i packages-microsoft-prod.deb",
-      "sudo apt-get update",
-      "sudo add-apt-repository universe",
-      "sudo apt-get install -y powershell",
-
-      # install docker
-      "sudo apt install -y apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable\"",
-      "sudo apt update",
-      "sudo apt install -y docker-ce",
-      "sudo usermod -aG docker agent",
-
-      # install and start the pipelines agent
-      "mkdir agent",
+      "sh /wait-for-cloud-init.sh",
       "cd agent",
-      "wget https://vstsagentpackage.azureedge.net/agent/2.159.2/vsts-agent-linux-x64-2.159.2.tar.gz",
-      "tar zxvf vsts-agent-linux-x64-2.159.2.tar.gz",
       "export VSTS_AGENT_INPUT_URL=https://dev.azure.com/g3rv4",
       "export VSTS_AGENT_INPUT_AUTH=pat",
       "export VSTS_AGENT_INPUT_TOKEN=${var.agent_pat}",
@@ -54,7 +28,6 @@ resource "digitalocean_droplet" "agent" {
       "./config.sh --unattended --acceptTeeEula --replace",
       "sudo ./svc.sh install",
       "sudo ./svc.sh start",
-      "EOF"
     ]
   }
 
